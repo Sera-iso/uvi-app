a0a0450e4ee93a538df773e4e860386fd2afa032
Sera <74925057+Sera-iso@users.noreply.github.com>
2022-06-29 01:03:00 +0200

    Refactor WeatherOverview component

--- src/WeatherOverview.js
+++ src/WeatherOverview.js
@@ -1,18 +1,30 @@
 import React from 'react'
-import UvIndex from './UvIndex'
+import UvIndexDisplay from './UvIndexDisplay'
 import Temperature from './Temperature'
 import DateTime from './DateTime'
 
+import './WeatherOverview.css'
+
 export default function WeatherOverview( {data} ) {
         let icon = `http://openweathermap.org/img/wn/${data.icon}.png`
-return(
-    <div className="WeatherOverview">
-        <h1>{data.city}, {data.country}</h1>
-        <UvIndex lon={data.lon} lat={data.lat} />
-        <Temperature celsius={Math.round(data.temp)} />
-        <img src={icon} alt={data.description} />
-        <p>{data.condition}</p>
-        <DateTime date={data.date}/>
-    </div>
-)
+
+    return(
+        <div className="WeatherOverview">
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <Temperature celsius={Math.round(data.temp)} />
+                    </div>
+                    <div className="col-3">
+                        <img src={icon} alt={data.description} />
+                    </div>
+                    <div className="col-6">
+                        <DateTime date={data.date}/>
+                    </div>
+                </div>
+            </div>
+            <p className="city">{data.city}, {data.country}</p>
+            <UvIndexDisplay lon={data.lon} lat={data.lat} />
+        </div>
+    )
 }
