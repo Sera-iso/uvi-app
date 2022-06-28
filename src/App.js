b2c6c09a89eee0e5a529fa371ffb0326647405c2
Sera <74925057+Sera-iso@users.noreply.github.com>
2022-06-29 00:58:07 +0200

    Added styles to the app and footer credits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Search from './Search'
-import ImageUploader from './ImageUploader'
+
 import './App.css';
 
 function App() {
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <Search defaultCity="Amsterdam" />
+      <header>
+        <h1>Check UV Index</h1>
       </header>
-        <ImageUploader />
+      <main>
+          <Search defaultCity="Amsterdam" />
+      </main>
+    </div>
+    <footer>
+      Coded with ❤️ by <a href="https://www.instagram.com/zee_codes/" target="_blank" rel="noreferrer">@zee_codes</a> and <a href="https://www.instagram.com/codewithsera/" target="_blank" rel="noreferrer">@codewithsera</a>
+    </footer>
     </div>
   );
 }
