a330127b67ad5e6b40a8779b651f508f0fc87c77
Sera <74925057+Sera-iso@users.noreply.github.com>
2022-06-29 08:10:01 +0200

    Update readme and footer with closing credits to the hackathon

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
       </main>
     </div>
     <footer>
-      Coded with ❤️ by <a href="https://www.instagram.com/zee_codes/" target="_blank" rel="noreferrer">@zee_codes</a> and <a href="https://www.instagram.com/codewithsera/" target="_blank" rel="noreferrer">@codewithsera</a>
+      Coded with ❤️ by <a href="https://www.instagram.com/zee_codes/" target="_blank" rel="noreferrer">@zee_codes</a> and <a href="https://www.instagram.com/codewithsera/" target="_blank" rel="noreferrer">@codewithsera</a><br />
+      as project for the Hackathon promoted by <a href="https://www.linode.com/" target="_blank" rel="noreferrer">Linode</a> and <a href="https://hashnode.com/" target="_blank" rel="noreferrer">Hashnode</a>
     </footer>
     </div>
   );
